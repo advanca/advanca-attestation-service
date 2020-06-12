c8eb2734674bad5fa42ef928a0603814ce7ec20a
Chua Zheng Leong <czl@iiyume.org>
2020-06-12 04:16:34 +0000

    Add sgx_enclave feature, build.rs add prelude

--- aas-protos/build.rs
+++ aas-protos/build.rs
@@ -1,3 +1,22 @@
+// Copyright 2020 ADVANCA PTE. LTD.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::fs;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+
 fn main() {
     let out_dir = "src/";
     let modules = &[("protos/aas", "aas")];
@@ -19,5 +38,37 @@ fn main() {
             .files(&files)
             .out_dir(&out_dir)
             .generate();
+        for str_path in files {
+            let output_path = Path::new(&out_dir);
+            let proto_path = Path::new(&str_path);
+            let mut filename = proto_path.file_stem().unwrap().to_os_string();
+            filename.push(".rs");
+            let wrapper_path = output_path.join(&filename);
+            add_prelude(&wrapper_path);
+        }
+    }
+}
+
+fn add_prelude(filepath: &Path) {
+    let old_content = fs::read_to_string(filepath).expect("Could not read file");
+    let content_split: Vec<&str> = old_content.split('\n').collect();
+
+    let mut new_content: Vec<String> = vec![];
+    // Alter content
+    for line in &content_split {
+        if line.starts_with("use protobuf::Message") {
+            new_content.push("#[cfg(feature = \"sgx_enclave\")]".into());
+            new_content.push("use sgx_tstd::prelude::v1::*;".into());
+        }
+        new_content.push(line.to_string());
+    }
+
+    // Store file
+    let mut file = match File::create(filepath) {
+        Err(_) => panic!("Could not open {:?}", filepath),
+        Ok(file) => file,
+    };
+    for i in &new_content {
+        writeln!(file, "{}", i).unwrap();
     }
 }
