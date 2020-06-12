a93991358deb6c0167c06b6815d9556ce6fd2922
Chua Zheng Leong <czl@iiyume.org>
2020-06-12 15:10:42 +0000

    Protos for enclave and std

---
+++ aas-protos/aas-protos-enclave/src/lib.rs
@@ -0,0 +1,11 @@
+#![cfg_attr(not(target_env = "sgx"), no_std)]
+#![cfg_attr(target_env = "sgx", feature(rustc_private))]
+
+#[cfg(not(target_env = "sgx"))]
+#[macro_use]
+extern crate sgx_tstd as std;
+
+//use protobuf_sgx as protobuf;
+extern crate protobuf_sgx as protobuf;
+
+pub mod aas;
